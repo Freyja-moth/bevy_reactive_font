d6385cd676e74c0e1989d6acb8b160aed6f5ccbf
Freyja-moth <freyjamoth@gmail.com>
2025-07-02 14:08:43 +0100

    Added more error variants

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{ecs::query::QueryEntityError, prelude::*};
 use thiserror::Error as ThisError;
 
 #[derive(ThisError, Debug)]
@@ -7,4 +7,8 @@ pub enum FontError {
         "Unable to find font for {text}. A font has not been specified and DefaultFont has not been set."
     )]
     CannotFindFont { text: Entity },
+    #[error("Entity {0} is not a FontCollection, why {1}")]
+    InvalidFont(Entity, QueryEntityError),
+    #[error("Entity {0}, is not a ReactiveFont, why {1}")]
+    InvalidReactiveFont(Entity, QueryEntityError),
 }
